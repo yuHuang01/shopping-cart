7a681cc6c619f3c25f75407855557605845c7889
yuHuang01 <huang.y0112@gmail.com>
2021-02-14 19:06:08 +0100

    Item listing in the shop is now working

--- src/App_components/Shop_components/ListShopItems.js
+++ src/App_components/Shop_components/ListShopItems.js
@@ -1,9 +1,16 @@
-import ShopItems from 'ShopItems.js';
+import ShopItems from './ShopItems'
 
 const ListShopItems = () => {
   return (
     <div id="ListShopItems">
-
+      {ShopItems.map(shopItem => {
+        return(
+          <div className="shop-items" key = { shopItem[1].id }>
+            <img src={`/imgs/board_game_imgs/${shopItem[1].src}`} alt={shopItem[0]}/>
+            <h3>{ shopItem[0] }</h3>
+          </div>
+        )
+      })}
     </div>
   );
 }
